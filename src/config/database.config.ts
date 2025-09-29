74b3865f512d7a383e8da6a23f680095da8cbd8a
chucomatnao <bkhaidinh@gmail.com>
2025-09-30 02:06:06 +0700

    Modified database.config.ts for MySQL connection

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ export default registerAs('database', () => ({
   host: process.env.DB_HOST ?? 'localhost',
   port: Number(process.env.DB_PORT ?? 3306),
   user: process.env.DB_USER ?? 'root',
-  pass: process.env.DB_PASS ?? '12345',
+  pass: process.env.DB_PASS ?? '123456',
   name: process.env.DB_NAME ?? 'mini_ecommerce',
   ssl: (process.env.DB_SSL ?? 'false') === 'true',
 }));
